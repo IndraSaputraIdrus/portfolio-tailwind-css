d2f12bdc727cb4facc67edb6ad00b2adad3489e2
indra saputra idrus <indrasaputrairus@gmail.com>
2022-04-04 18:46:55 +0800

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "16px",
+      },
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
